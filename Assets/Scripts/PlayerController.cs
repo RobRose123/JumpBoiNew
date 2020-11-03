f6cfbfd7816ce917970f551995d8505ca33ff265
robrose02 <55984669+robrose02@users.noreply.github.com>
2020-11-03 13:38:20 +0000

    Start of project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    
+
+
+    //Ground / Inputcheck
+    private float moveInput;
+    private bool isGrounded;
+    public Transform feetPos;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    //Jump
+    private float jumpTimeCounter;
+    public float jumpForce;
+    public float jumpTime;
+    private bool isJumping;
+    private bool facingRight = true;
+
+    //Dash
+    public float dashSpeed = 30f;
+    public float startDashTime = 0.1f;
+    private float dashTime;
+    private int direction;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        //Dash
+        dashTime = startDashTime;
+    }
+
+    private void Update()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+        void Flip()
+        {
+            facingRight = !facingRight;
+
+            transform.Rotate(0f, 180f, 0f);
+        }
+
+        if (direction == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                if (moveInput < 0)
+                {
+                    direction = 1;
+                }
+                else if (moveInput > 0)
+                {
+                    direction = 2;
+                }
+            }
+        }
+        else
+        {
+            if (dashTime <= 0)
+            {
+                direction = 0;
+                dashTime = startDashTime;
+                rb.velocity = Vector2.zero;
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+
+                if (direction == 1)
+                {
+                    rb.velocity = Vector2.left * dashSpeed;
+                }
+                else if (direction == 2)
+                {
+                    rb.velocity = Vector2.right * dashSpeed;
+                }
+            }
+        }
+    }
+}
